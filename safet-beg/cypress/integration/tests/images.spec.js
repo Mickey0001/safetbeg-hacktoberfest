e8cbb7796be0826a8c3e4f309ea7581e06316d7d
Mirza Sisic <84443026+mirzonius@users.noreply.github.com>
2021-10-15 15:40:51 +0200

    Add tests for images on other pages

--- safet-beg/cypress/integration/tests/images.spec.js
+++ safet-beg/cypress/integration/tests/images.spec.js
@@ -5,5 +5,15 @@ it('verifies images are present', ()=> {
     cy.visit('http://localhost:3000/')
 
     cy.get('img').should('be.visible')
+
+
+    cy.visit('http://localhost:3000/works/bururet')
+
+    cy.get('img').should('be.visible')
+
+    cy.visit('http://localhost:3000/contact')
+
+    cy.get('img').should('be.visible')
+
   })
   
