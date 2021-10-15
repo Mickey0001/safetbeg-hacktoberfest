035c53d3ac97c6ec924b1030e0b55670114db111
Mirza Sisic <84443026+mirzonius@users.noreply.github.com>
2021-10-15 15:57:20 +0200

    SB-26 trying out the Jira integration

---
+++ safet-beg/cypress/integration/tests/images.spec.js
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+it('verifies images are present', ()=> {
+
+    cy.visit('http://localhost:3000/')
+
+    cy.get('img').should('be.visible')
+
+
+    //add tests for other sub-pages when they're added
+    cy.visit('http://localhost:3000/works/bururet')
+
+    cy.get('img').should('be.visible')
+
+    cy.visit('http://localhost:3000/contact')
+
+    cy.get('img').should('be.visible')
+
+  })
+  
