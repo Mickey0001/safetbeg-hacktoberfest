f447f6e9f594c86431a246aa12ed5e471065e9b9
Mirza Sisic <84443026+mirzonius@users.noreply.github.com>
2021-10-19 17:12:08 +0200

    Fix merge conflict

--- safet-beg/cypress/integration/tests/images.spec.js
+++ safet-beg/cypress/integration/tests/images.spec.js
@@ -6,11 +6,6 @@ it('verifies images are present', ()=> {
 
     cy.get('img').should('be.visible')
 
-
-<<<<<<< HEAD
-    //add tests for other sub-pages when they're added
-=======
->>>>>>> cf0e886639ba5807518358d6cfe898011e27bc0e
     cy.visit('http://localhost:3000/works/bururet')
 
     cy.get('img').should('be.visible')
