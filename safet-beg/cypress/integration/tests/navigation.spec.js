505586515984cdb525a77c231f44683f12cc2ab2
Mirza Sisic <84443026+mirzonius@users.noreply.github.com>
2021-10-29 16:49:41 +0200

    Open base URL before each test Groupe tests in mocha describe Spearate navbar tests

--- safet-beg/cypress/integration/tests/navigation.spec.js
+++ safet-beg/cypress/integration/tests/navigation.spec.js
@@ -1,31 +1,33 @@
 /// <reference types="cypress" />
 
-describe('link tests', () => {
-  cy.visit('http://localhost:3000/')
-  cy.get('button').click()
+  describe('opens all nav links', () => {
 
-  it('opens all nav links', () => {
+    beforeEach(() => {
+      cy.visit('http://localhost:3000/')
+    })
 
     it('should open the home page', () => {
+      cy.get('button').click()
       cy.contains('Home').click()
       cy.location('pathname').should('equal', '/')
     })
 
     it('should open the about page', () => {
+      cy.get('button').click()
       cy.contains('About').click()
       cy.location('pathname').should('equal', '/about')
     })
 
     it('should open the works page', () => {
+      cy.get('button').click()
       cy.contains('Works').click()
       cy.location('pathname').should('equal', '/works/bururet')
     })
 
     it('should open the contact page', () => {
+      cy.get('button').click()
       cy.contains('Contact').click()
       cy.location('pathname').should('equal', '/contact')
     })
 
-
-  })
 })
