c550d1cebdbafcc8ef1b96c424999ef50ac0bf11
Mirza Sisic <84443026+mirzonius@users.noreply.github.com>
2021-10-29 16:35:57 +0200

    Group navigation tests

--- safet-beg/cypress/integration/tests/navigation.spec.js
+++ safet-beg/cypress/integration/tests/navigation.spec.js
@@ -1,23 +1,31 @@
 /// <reference types="cypress" />
 
-it('opens all nav links', ()=> {
-
+describe('link tests', () => {
   cy.visit('http://localhost:3000/')
   cy.get('button').click()
 
-   //home page
-   cy.contains('Home').click()
-   cy.location('pathname').should('equal', '/')
+  it('opens all nav links', () => {
 
-    //about page
-    cy.contains('About').click()
-    cy.location('pathname').should('equal', '/about')
+    it('should open the home page', () => {
+      cy.contains('Home').click()
+      cy.location('pathname').should('equal', '/')
+    })
 
-    //works page
-    cy.contains('Works').click()
-    cy.location('pathname').should('equal', '/works/bururet')
-  
-    //contact page
-    cy.contains('Contact').click()
-    cy.location('pathname').should('equal', '/contact') 
+    it('should open the about page', () => {
+      cy.contains('About').click()
+      cy.location('pathname').should('equal', '/about')
+    })
+
+    it('should open the works page', () => {
+      cy.contains('Works').click()
+      cy.location('pathname').should('equal', '/works/bururet')
+    })
+
+    it('should open the contact page', () => {
+      cy.contains('Contact').click()
+      cy.location('pathname').should('equal', '/contact')
+    })
+
+
+  })
 })
