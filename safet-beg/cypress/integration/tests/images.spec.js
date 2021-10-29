1ccdcf43ed055e0e8d424cc6975180b8fc07306e
Mirza Sisic <84443026+mirzonius@users.noreply.github.com>
2021-10-29 17:02:27 +0200

    Group tests for images within a Describe Separate each individual test to separate It function

--- safet-beg/cypress/integration/tests/images.spec.js
+++ safet-beg/cypress/integration/tests/images.spec.js
@@ -1,18 +1,20 @@
 /// <reference types="cypress" />
 
-it('verifies images are present', ()=> {
+describe('verifies images are present', () => {
 
+  it('should open the home image', () => {
     cy.visit('http://localhost:3000/')
-
     cy.get('img').should('be.visible')
-
-    cy.visit('http://localhost:3000/works/bururet')
-
-    cy.get('img').should('be.visible')
-
-    cy.visit('http://localhost:3000/contact')
-
-    cy.get('img').should('be.visible')
-
   })
-  
+
+  it('should open the burruret image', () => {
+    cy.visit('http://localhost:3000/works/bururet')
+    cy.get('img').should('be.visible')
+  })
+
+  it('should show contact image', () => {
+    cy.visit('http://localhost:3000/contact')
+    cy.get('img').should('be.visible')
+  })
+
+})
